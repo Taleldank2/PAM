e26dfd6351b494773b552eb582e38d876eb87fa8
Tal Eldan <33514645+Taleldank2@users.noreply.github.com>
2018-03-07 13:57:05 +0200

    Changed Classes folder location

---
+++ WEBAPP/Classes/Classes/Attendance.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Attendance
+    {
+
+        //Fields
+        private int eventNumber, athleteNumber;
+        private bool present;
+        private string remark;
+
+        //Ctors
+        public Attendance()
+        {
+
+        }
+        public Attendance(int eventNumber, int athleteNumber, bool present, string remark)
+        {
+            this.eventNumber = eventNumber;
+            this.athleteNumber = athleteNumber;
+            this.present = present;
+            this.remark = remark;
+        }
+
+        //Props
+        public int AthleteNumber
+        {
+            get
+            {
+                return athleteNumber;
+            }
+
+            set
+            {
+                athleteNumber = value;
+            }
+        }
+
+        public int EventNumber
+        {
+            get
+            {
+                return eventNumber;
+            }
+
+            set
+            {
+                eventNumber = value;
+            }
+        }
+
+        public bool Present
+        {
+            get
+            {
+                return present;
+            }
+
+            set
+            {
+                present = value;
+            }
+        }
+
+        public string Remark
+        {
+            get
+            {
+                return remark;
+            }
+
+            set
+            {
+                remark = value;
+            }
+        }
+
+    }
+}
---
+++ WEBAPP/Classes/Classes/Event.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Event
+    {
+
+        //Fields
+        private int teamNumber, eventKind, eventNumber;
+        private string title, body, eventDate, startTime, endTime, loction, remark;
+        private bool recurrent;
+
+        //Ctors
+        public Event()
+        {
+
+        }
+        public Event(int teamNumber, int eventKind, string title, string body, string eventDate, string startTime, string endTime, string loction, bool recurrent, string remark)
+        {
+            this.TeamNumber = teamNumber;
+            this.EventKind = eventKind;
+            this.Title = title;
+            this.Body = body;
+            this.EventDate = eventDate;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.Loction = loction;
+            this.Remark = remark;
+            this.Recurrent = recurrent;
+        }
+
+        //Props
+        public string Body
+        {
+            get
+            {
+                return body;
+            }
+
+            set
+            {
+                body = value;
+            }
+        }
+
+        public string EndTime
+        {
+            get
+            {
+                return endTime;
+            }
+
+            set
+            {
+                endTime = value;
+            }
+        }
+
+        public string EventDate
+        {
+            get
+            {
+                return eventDate;
+            }
+
+            set
+            {
+                eventDate = value;
+            }
+        }
+
+        public int EventKind
+        {
+            get
+            {
+                return eventKind;
+            }
+
+            set
+            {
+                eventKind = value;
+            }
+        }
+
+        public int EventNumber
+        {
+            get
+            {
+                return eventNumber;
+            }
+
+            set
+            {
+                eventNumber = value;
+            }
+        }
+
+        public string Loction
+        {
+            get
+            {
+                return loction;
+            }
+
+            set
+            {
+                loction = value;
+            }
+        }
+
+        public string Remark
+        {
+            get
+            {
+                return remark;
+            }
+
+            set
+            {
+                remark = value;
+            }
+        }
+
+        public string StartTime
+        {
+            get
+            {
+                return startTime;
+            }
+
+            set
+            {
+                startTime = value;
+            }
+        }
+
+        public int TeamNumber
+        {
+            get
+            {
+                return teamNumber;
+            }
+
+            set
+            {
+                teamNumber = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public bool Recurrent
+        {
+            get
+            {
+                return recurrent;
+            }
+
+            set
+            {
+                recurrent = value;
+            }
+        }
+    }
+}
