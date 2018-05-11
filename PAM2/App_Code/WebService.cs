2c51b0c5b4bc587cd32ce51d938692d08f03b9e8
talkaf1 <37043622+talkaf1@users.noreply.github.com>
2018-05-11 13:18:39 +0300

    try catch db services

--- PAM2/App_Code/WebService.cs
+++ PAM2/App_Code/WebService.cs
@@ -229,14 +229,14 @@ public class WebService : System.Web.Services.WebService
         w.WriteLine("-------------------------------");
     }
 
-    public static void DumpLog(StreamReader r)
-    {
-        string line;
-        while ((line = r.ReadLine()) != null)
-        {
-            Console.WriteLine(line);
-        }
-    }
+    //public static void DumpLog(StreamReader r)
+    //{
+    //    string line;
+    //    while ((line = r.ReadLine()) != null)
+    //    {
+    //        Console.WriteLine(line);
+    //    }
+    //}
 
     //--------------------------------------------------------------------
     //                           Dashboard
