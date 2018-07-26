f1ae384de2f6cb7b3f1bd53cff06609a1bf7c843
Tal Eldan <33514645+Taleldank2@users.noreply.github.com>
2018-07-26 09:25:16 +0300

    myApp9

---
+++ myApp9/platforms/browser/www/App_Code/WebService.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+// REMEMBER TO ADD THIS NAMESPACE
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Runtime.Remoting.Contexts;
+using System.Web.Configuration;
+using System.Data;
+
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+
+public class WebService : System.Web.Services.WebService
+{
+    static Dictionary<string, string> usersSessions = new Dictionary<string, string>();
+    private DBServices dbHandler;
+
+    public WebService()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+
+        string connectionString = WebConfigurationManager.ConnectionStrings["PamDBconnectionString"].ConnectionString;
+        dbHandler = new DBServices(connectionString);
+    }
+
+    //--------------------------------------------------------------------
+    //                           Profile
+    //--------------------------------------------------------------------
+    [WebMethod]
+    public void update(string UserID, string phoneNumber, string userMail, string userPassword, string city, string athleteWeight, string athleteHeight)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        dbHandler.updateDetails(UserID, phoneNumber, userMail, userPassword, city, athleteWeight, athleteHeight);
+    }
+
+    //--------------------------------------------------------------------
+    //                           Login
+    //--------------------------------------------------------------------
+    [WebMethod]
+    public string Login(string phoneNumber, string password)
+    {
+        //Create New User obj
+        User myUser = new User();
+        try
+        {
+            //Search User in DataBase
+            DataTable dt = dbHandler.getUser(phoneNumber);
+
+            //If user does not exist in database
+            if (dt.Rows[0][0] == null)
+            {
+                // serialize to string
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string jsonString = js.Serialize("שם משתמש לא קיים במערכת");
+                return jsonString;
+            }
+            else
+            {
+                //User Exist in DataBase
+                //Check Password                               
+                if (dt.Rows[0][6].ToString() == password)
+                {
+                    //Password is approved
+                    myUser.UserId = dt.Rows[0][0].ToString();
+                    myUser.UserType = dt.Rows[0][1].ToString();
+                    myUser.FirstName = dt.Rows[0][2].ToString();
+                    myUser.LastName= dt.Rows[0][3].ToString();
+                    myUser.PhoneNumber = dt.Rows[0][4].ToString();
+                    myUser.Email = dt.Rows[0][5].ToString();
+                    myUser.Password = dt.Rows[0][6].ToString();
+                    myUser.Picture = dt.Rows[0][7].ToString();
+                    myUser.City = dt.Rows[0][8].ToString();
+                    myUser.BirthDate = dt.Rows[0][9].ToString();
+
+                    try
+                    {
+                        ////Create GUID number
+                        //string userGuid = Guid.NewGuid().ToString();
+
+                        ////get the user id
+                        //string userId = dt.Rows[0][0].ToString();
+
+                        //// Search inside userSession dictionary table if user is there 
+                        //if (usersSessions.Keys.Contains(userId))
+                        //{
+                        //    usersSessions[userId] = userGuid;
+                        //}
+                        //else
+                        //{
+                        //    //if user is not there, add him to the dictionary
+                        //    usersSessions.Add(userId, userGuid);
+                        //}
+
+                        ////Give the user cookie named "session" and give it the value of the user guid for example:
+                        ////"session=4564564-5646-456-4564"
+                        //HttpCookie cookie = new HttpCookie("session");
+                        //cookie["session"] = userGuid;
+
+                        //cookie.Expires = DateTime.Now.AddHours(1);
+
+                        //Context.Response.Cookies.Add(cookie);
+                        
+                        // serialize to string
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        string jsonString = "";
+                        jsonString = js.Serialize(myUser);
+                        return jsonString;
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        // serialize to string
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        string jsonString = js.Serialize("Session table error (Go to WebService class to debug!): " + ex.Message);
+                        return jsonString;
+                    }
+                }
+                else
+                {
+                    //Password not approved
+                    // serialize to string
+                    JavaScriptSerializer js = new JavaScriptSerializer();
+                    string jsonString = js.Serialize("סיסמה שגויה");
+                    return jsonString;
+                }
+
+            }
+        }
+
+        catch (Exception ex)
+        {
+            // serialize to string
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonString = js.Serialize("Unable to read from the database" + ex.Message);
+            return jsonString;
+            
+        }
+
+    }
+
+    [WebMethod]
+    public string checkUserSession(string userSession) //user session is the GUID string that is being send from:
+                                                       //checkUserExists ajax call
+                                                       //this method is being called on every page load
+    {
+        string response = "false";
+
+        if (usersSessions.Values.Contains(userSession))
+        {
+            response = "true";
+        }
+
+        return response;
+    }
+
+    [WebMethod]
+    private string getUserFromSession(string sessionId)//Extract the user id from the session table
+    {
+        foreach (KeyValuePair<string, string> pair in usersSessions)
+        {
+            if (pair.Value == sessionId)
+            {
+                return pair.Key;
+            }
+        }
+
+        throw new Exception("Invalid session id");
+    }
+
+    [WebMethod]
+    public string getUser(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userID = getUserFromSession(userSession);
+
+        DataTable details = dbHandler.getUser(userId);
+
+        string response = dataTableToJson(details);
+
+        return response;
+
+    }
+
+
+    [WebMethod]
+    public void addUserReg (string userId,string regId)
+    {
+        dbHandler.addUserReg(userId, regId);
+            
+    }
+
+    //[WebMethod]
+    //public string getUserType(string userID)
+    //{
+    //    //string userSession = Context.Request.Cookies["session"]["session"];
+
+    //    //string userID = getUserFromSession(userSession);
+
+    //    DataTable details = dbHandler.getUserType(userID);
+
+    //    string response = dataTableToJson(details);
+
+    //    return response;
+
+    //}//Get user object from session
+
+
+    //--------------------------------------------------------------------
+    //                           Register
+    //--------------------------------------------------------------------
+
+    [WebMethod]
+    public void register(string userType,string userTeam, string userName, string userLastName, string phoneNumber,
+                           string userMail, string userPassword, string userPicBase64, string city,
+                           string userBirthday, string athleteWeight, string athleteHeight)
+    {
+        dbHandler.registerUser(userType, userName, userLastName, phoneNumber, userMail, userPassword,
+                               userPicBase64, city, userBirthday, userTeam, athleteWeight, athleteHeight);
+
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        //dbHandler.registerAthlete(userId, userTeam, athleteWeight, athleteHeight);
+
+    }
+
+    [WebMethod]
+    public string getTeamsNames()
+    {
+
+        DataTable TeamNames = dbHandler.getTeamsNames();
+
+        string response = dataTableToJson(TeamNames);
+
+        return response;
+    }
+
+    [WebMethod]
+    public string getPicturePath(string userId)
+    {
+
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        string picturePath = dbHandler.getPicturePath(userId);
+
+        return picturePath;
+    }
+
+
+    //--------------------------------------------------------------------
+    //                           Main Page
+    //--------------------------------------------------------------------
+
+    [WebMethod]
+    public string getUserLastEvent(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        DataTable events = dbHandler.getUserLastEvent(userId);
+
+        string response = dataTableToJson(events);
+
+        return response;
+    }
+
+    [WebMethod]
+    public string getScore(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        string score = dbHandler.getUserScore(userId);
+
+        return score;
+    }
+
+    [WebMethod]
+    public string getName(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        string name = dbHandler.getUserName(userId);
+
+        return name;
+    }
+
+    [WebMethod]
+    public string getLastResult(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        DataTable result = dbHandler.getUserLastResult(userId);
+
+        string response = dataTableToJson(result);
+
+        return response;
+    }
+
+    [WebMethod]
+    public string getMessagesCount(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        string messagesCount = dbHandler.getMessagesCount(userId);
+
+        return messagesCount;
+    }
+
+
+    //--------------------------------------------------------------------
+    //                           App Pages
+    //--------------------------------------------------------------------
+
+    [WebMethod]
+    public string getUserDetails(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        DataTable details = dbHandler.getDetails(userId);
+
+        string response = dataTableToJson(details);
+
+        return response;
+    }
+
+    [WebMethod]
+    public string getUserResults(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        DataTable result = dbHandler.getUserResults(userId);
+
+        string response = dataTableToJson(result);
+
+        return response;
+    }
+
+    [WebMethod]
+    public string getUserMessages(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        DataTable messages = dbHandler.getUserMessages(userId);
+
+        string response = dataTableToJson(messages);
+
+        return response;
+    }
+
+    [WebMethod]
+    public string getUserEvents(string userId)
+    {
+        //string userSession = Context.Request.Cookies["session"]["session"];
+
+        //string userId = getUserFromSession(userSession);
+
+        DataTable events = dbHandler.getUserEvents(userId);
+
+        string response = dataTableToJson(events);
+
+        return response;
+
+    }
+
+    //[WebMethod]
+    //public string getCoachEvents()
+    //{
+    //    string userSession = Context.Request.Cookies["session"]["session"];
+
+    //    string userId = getUserFromSession(userSession);
+
+    //    DataTable events = dbHandler.getCoachEvents(userId);
+
+    //    string response = dataTableToJson(events);
+
+    //    return response;
+
+    //}
+
+    //--------------------------------------------------------------------
+    //                           Snippets
+    //--------------------------------------------------------------------
+
+    private string dataTableToJson(DataTable table)
+    {
+        JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+        List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+        Dictionary<string, object> childRow;
+        foreach (DataRow row in table.Rows)
+        {
+            childRow = new Dictionary<string, object>();
+            foreach (DataColumn col in table.Columns)
+            {
+                childRow.Add(col.ColumnName, row[col]);
+            }
+            parentRow.Add(childRow);
+        }
+
+        return jsSerializer.Serialize(parentRow);
+    }
+
+
+}
+
+
+
+
+
+
---
+++ myApp9/www/js/Register.js
@@ -0,0 +1,237 @@
+﻿document.getElementById("myForm").onsubmit = register;
+
+
+//function getUserId() {
+//    userIdNum = localStorage["UserID"];
+//    request = {
+//        userId: userIdNum
+//    };
+//    return request;
+//}
+
+function getMoreDetails() {
+    getTeamsNames();
+    AthleteDetails();
+    if (localStorage["UserDetails"] == "true") {
+        $('#phonenumber').val(localStorage.UserNumber);
+        $('#userteam').attr(localStorage.UserTeam);
+        $('#name').val(localStorage.UserName);
+        $('#lastname').val(localStorage.UserLastName);
+        $('#mail').val(localStorage.UserMail);
+        $('#city').val(localStorage.UserCity);
+        $('#datepicker').val(localStorage.UserDate);
+        $('#AthleteWeight').val(localStorage.UserWeight);
+        $('#AthleteHeight').val(localStorage.UserHeight);
+        $('#picture').val(localStorage.UserPic);
+
+    }
+}
+
+function AthleteDetails() {
+    str = "<div class='form-group'><div class='input-group'>" +
+"<div class='input-group-prepend'><span class='input-group-text'>" +
+"<i class='mdi mdi-weight-kilogram'></i></span></div>" +
+"<input id='AthleteWeight' class='form-control' type='text' required='' placeholder='משקל'></div></div>";
+
+    str += "<div class='form-group'><div class='input-group'>" +
+    "<div class='input-group-prepend'><span class='input-group-text'>" +
+    "<i class='mdi mdi-ruler'></i></span></div>" +
+    "<input id='AthleteHeight' class='form-control' type='text' required='' placeholder='גובה במטרים (לדוגמא: 1.68)'></div></div>";
+
+    $('#AthleteFileds').append(str);
+
+}
+
+function getTeamsNames() {
+
+    $.ajax({
+        url: ASMXURL + 'getTeamsNames',
+        type: 'POST',
+        async: true,
+        dataType: 'json',
+        contentType: 'application/json; charset = utf-8',
+        success: getTeamsNamesCompleted,
+        error: getTeamsNamesFailed
+    })
+}
+
+function getTeamsNamesCompleted(results) {
+    results = $.parseJSON(results.d);
+    str = "<div class='form-group'><div class='input-group'><div class='input-group-prepend'>" +
+                    "<span class='input-group-text'><i class='mdi mdi-swim'></i></span></div>" +
+                    "<select id='userteam' class='form-control select2 select2-hidden-accessible' tabindex='-1'" +
+                    "aria-hidden='true' ><option value='' disabled selected> בחר קבוצה </option> ";
+    for (var i = 0; i < results.length; i++) {
+        str += "<option value=" + results[i].TeamID + " >" + results[i].TeamName + "</option>";
+    }
+    str += "</select></div> </div>";
+    $('#TeamsNamesDDL').append(str);
+}
+
+function getTeamsNamesFailed() {
+    alert("שמות הקבוצה לא יובאו");
+}
+
+function register() {
+    try {
+        if (localStorage["UserDetails"] != "true") {
+            localStorage["UserDetails"] = "true";
+            localStorage["UserNumber"] = $('#phonenumber').val();
+            localStorage["UserTeam"] = $('#userteam').val();
+            localStorage["UserName"] = $('#name').val();
+            localStorage["UserLastName"] = $('#lastname').val();
+            localStorage["UserMail"] = $('#mail').val();
+            localStorage["UserCity"] = $('#city').val();
+            localStorage["UserDate"] = $('#datepicker').val();
+            localStorage["UserWeight"] = $('#AthleteWeight').val();
+            localStorage["UserHeight"] = $('#AthleteHeight').val();
+            localStorage["UserPic"] = $('#picture')[0].files[0];
+        }
+
+        isChecked = document.getElementById("checkbox-signup").checked;
+        if (isChecked == false) {
+            alert("תחילה יש לאשר את תנאי השימוש");
+            return;
+        }
+
+        if (($('#phonenumber').val()).length != 10) {
+            alert("יש להכניס מספר נייד בעל עשר ספרות ללא רווחים וסימנים");
+            localStorage.removeItem("UserNumber");
+            return;
+        }
+
+        userPassword = $('#pass').val();
+        userConfirmPassword = $('#passConfirm').val();
+        if (userPassword != userConfirmPassword) {
+            alert("אימות סיסמא נכשל!")
+            return;
+        }
+
+        if (isNaN(parseInt($('#AthleteWeight').val())) ){
+            alert("יש להכניס משקל עצמי כמספר עגול");
+            localStorage.removeItem("UserWeight");
+            return;
+        }
+        if (parseInt($('#AthleteWeight').val()) < 35 || parseInt($('#AthleteWeight').val()) > 150) {
+            alert("יש להכניס את המשקל האמיתי :)");
+            localStorage.removeItem("UserWeight");
+            return;
+        }
+
+        if (parseInt($('#AthleteHeight').val()) != 1 ) {
+            alert("יש להכניס גובה במטרים, למשל: 1.72");
+            localStorage.removeItem("UserHeight");
+            return;
+        }
+
+        file = $('#picture')[0].files[0];
+        if (file == null) {
+            localStorage.removeItem("UserPic");
+            alert("לא תברח/י מזה ! יש להכניס תמונה");
+            return;
+        }
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            registerNewUser(e.target.result)
+        };
+
+        reader.readAsDataURL(file);
+    }
+    catch (e) {
+        alert("תהליך נכשל !\n יש לפנות למנהל מעכת");
+    }
+
+}
+
+
+function registerNewUser(pictureBase64) {
+
+    userType = 1;
+    userTeam = $('#userteam').val();
+    userName = $('#name').val();
+    userLastName = $('#lastname').val();
+    userPhoneNumber = $('#phonenumber').val();
+    userMail = $('#mail').val();
+    userPassword = $('#pass').val();
+    userPicture = pictureBase64;
+    userCity = $('#city').val();
+    userBirthday = $('#datepicker').val();
+    athleteWeight = $('#AthleteWeight').val();
+    athleteHeight = $('#AthleteHeight').val();
+
+    request = {
+        "userType": userType,
+        "userName": userName,
+        "userLastName": userLastName,
+        "phoneNumber": userPhoneNumber,
+        "userMail": userMail,
+        "userPassword": userPassword,
+        "userPicBase64": userPicture,
+        "city": userCity,
+        "userBirthday": userBirthday,
+        "userTeam": userTeam,
+        "athleteWeight": athleteWeight,
+        "athleteHeight": athleteHeight
+
+    };
+
+    var dataString = JSON.stringify(request);
+
+    //alert(dataString);
+
+    $.ajax({
+        url: ASMXURL + 'register',
+        data: dataString,
+        type: 'POST',
+        async: false,
+        dataType: 'json',
+        contentType: 'application/json; charset = utf-8',
+        success: registerCompleted,
+        error: errorCB
+    })
+
+    //userID = getUserId();
+
+    //requestAthlete = {
+    //    "userId": userID,
+    //    "userTeam": userTeam,
+    //    "athleteWeight": athleteWeight,
+    //    "athleteHeight": athleteHeight
+    //};
+
+    //var dataStringAthlete = JSON.stringify(requestAthlete);
+
+
+    //$.ajax({
+    //    url: ASMXURL + 'registerAthlete',
+    //    data: dataString,
+    //    type: 'POST',
+    //    async: false,
+    //    dataType: 'json',
+    //    contentType: 'application/json; charset = utf-8',
+    //    success: registerCompleted,
+    //    error: errorCB
+    //})
+
+    localStorage.removeItem("UserDetails");
+    localStorage.removeItem("UserNumber");
+    localStorage.removeItem("UserTeam");
+    localStorage.removeItem("UserName");
+    localStorage.removeItem("UserLastName");
+    localStorage.removeItem("UserMail");
+    localStorage.removeItem("UserCity");
+    localStorage.removeItem("UserDate");
+    localStorage.removeItem("UserWeight");
+    localStorage.removeItem("UserHeight");
+    localStorage.removeItem("UserPic");
+}
+
+function registerCompleted(result) {
+    alert("ההרשמה בוצעה בהצלחה :)");
+    window.location = "Login.html";
+}
+
+function errorCB(e) {
+    alert("The exception message is : " + e.responseText);
+}
