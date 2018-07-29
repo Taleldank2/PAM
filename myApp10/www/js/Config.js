1bfbc5a677aef3ab2b13d043a840a12e089cc7c2
talkaf1 <37043622+talkaf1@users.noreply.github.com>
2018-07-29 04:49:14 +0300

    local myapp10

--- myApp10/www/js/Config.js
+++ myApp10/www/js/Config.js
@@ -8,5 +8,5 @@ var local = false;
 
 var ASMXURL = 'http://localhost:31629/WebService.asmx/';
 if (!local) {
-    ASMXURL = 'http://proj.ruppin.ac.il/bgroup57/prod/Code/pamAthlete/WebService.asmx/';
+    ASMXURL = 'http://proj.ruppin.ac.il/bgroup57/test1/tar1/WebService.asmx/';
 }
